736b505aaf3185eec7d9318da1ce2c235fa5bb4b
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-10-27 22:10:24 +1100

    connection to db and userschemas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // connectDB
 
+const connectDB = require("./db/connect");
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -32,6 +33,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
